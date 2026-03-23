e77b8e0fd0b3aaaa35c39d3690ec9d20e290915d
MasterHadiah <117649115+MasterHadiah@users.noreply.github.com>
2026-03-23 21:41:59 +0100

    Update fetch-all.js

--- fetch-all.js
+++ fetch-all.js
@@ -24,7 +24,12 @@ function getWithCookies(urlPath, cookieStr) {
       hostname: 'web.eduflexcloud.nl',
       path: urlPath,
       method: 'GET',
-      headers: { 'Cookie': cookieStr }
+      headers: {
+        'Cookie': cookieStr,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'nl-NL,nl;q=0.9',
+      }
     };
     const req = https.request(opts, res => {
       let d = ''; res.on('data', c => d += c);
