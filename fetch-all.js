a9ed90da51065a79f43f08d41c993b3f9e394153
MasterHadiah <117649115+MasterHadiah@users.noreply.github.com>
2026-03-23 21:44:36 +0100

    Update fetch-all.js

--- fetch-all.js
+++ fetch-all.js
@@ -49,8 +49,11 @@ function post(urlPath, data, cookie) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(postData),
-        'Cookie': cookie || '',
+        'Content-Length': Buffer.byteLength(loginData),
+        'Cookie': cookieStr(jar),
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'nl-NL,nl;q=0.9',
       }
     };
     const req = https.request(opts, res => {
